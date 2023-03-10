a0b956c8fb65a5b260aa541e23cec8190c1eb6d6
Jose Alonso <jose.m.alonso.g@gmail.com>
2023-03-10 19:37:58 +0000

    deleted all

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import type { AppProps } from "next/app";
-
-import "@/styles/global.css";
-
-const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
-};
-
-export default App;
