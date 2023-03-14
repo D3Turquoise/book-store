60296f8e3363315d3c4425b3501636e12f9515ff
D3Turquoise <anastasija_toleva@yahoo.com>
2023-03-14 20:43:11 +0000

    creating components, searchbar and header

---
+++ src/component/Searchbar.jsx
@@ -0,0 +1,31 @@
+
+export default function Search() {
+    return (
+<div class="flex justify-center">
+  <div class="mb-3 xl:w-96">
+    <div class="relative mb-4 flex w-full flex-wrap items-stretch">
+      <input
+        type="search"
+        class="relative m-0 block w-[1%] min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-1.5 text-base font-normal text-neutral-700 outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 dark:placeholder:text-neutral-200"
+        placeholder="Search"
+        aria-label="Search"
+        aria-describedby="button-addon2" />
+      <span
+        class="input-group-text flex items-center whitespace-nowrap rounded px-3 py-1.5 text-center text-base font-normal text-neutral-700 dark:text-neutral-200"
+        id="basic-addon2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          class="h-5 w-5">
+          <path
+            fill-rule="evenodd"
+            d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+            clip-rule="evenodd" />
+        </svg>
+      </span>
+    </div>
+  </div>
+</div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
+import Header from '../component/Header'
+import Search from '../component/Searchbar'
+import Toprated from '@/component/Toprated'
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <>
+      <div className="m-5">
+    <Search/>
+    </div>
+    <Header/>
+
+      </>
   )
 }
