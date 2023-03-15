9b058398bd517b584222a216c2e00afa92b35707
D3Turquoise <anastasija_toleva@yahoo.com>
2023-03-15 19:24:54 +0000

    adding footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,21 @@
 import Header from '../component/Header'
 import Search from '../component/Searchbar'
-import Toprated from '@/component/Toprated'
+import Toprated from '../component/Toprated'
+import Footer from '../component/Footer'
 
 export default function Home() {
   return (
-   <>
+    <>
       <div className="m-5">
-    <Search/>
-    </div>
-    <Header/>
-    <div className='m-5'>
-    <Toprated/>
-    </div>
-      </>
+        <Search />
+      </div>
+      <Header />
+      <div className='m-5'>
+        <Toprated />
+      </div>
+      <div className='m-5'>
+        <Footer />
+      </div>
+    </>
   )
 }
